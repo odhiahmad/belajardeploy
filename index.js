1e556f2962d9a0cf2b984da0f824a8d23b2a1daf
odhiahmad <reservaindonesiagg@gmail.com>
2022-03-06 18:29:22 +0700

    test test

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const port = process.env.port || 8000;
+const express = require("express");
+const port = process.env.port || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => { console.log('Our App Is Up And Running!'); });
-module.exports = app
+app.listen(port, () => {
+  console.log("Our App Is Up And Running!");
+});
+module.exports = app;
